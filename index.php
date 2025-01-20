a896da014bd5d0c875f445454f0ca23807374a70
Ivnkaramazov <ivand.rapzoda@gmail.com>
2025-01-20 16:27:41 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ChessHr</title>
+        <meta name="description" content="">
+<link href="Css/Estilo1.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<p><div class="panelx">	<img src="Soporte/logochess.png"></p><br>
+	<button class="btn_1" onclick="location.href='Controlador/login.php'">Curso 1</button>
+	<button class="btn_1" onclick="location.href='Controlador/login.php'">Curso 2</button>
+	<button class="btn_1" onclick="location.href='Soporte/nodisponible.php'">Curso 3</button>
+	<button class="btn_1" onclick="location.href='Soporte/nodisponible.php'">Curso 4</button>
+	<h3>"La jugada está ahí, solo necesitas verla."</h3></div>
+</body>
+</html>
